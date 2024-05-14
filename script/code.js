73709994d3bb093ff8aec335b2314321df4a9688
Name <anushqakandan03@gmail.com>
2024-05-14 15:42:55 +0200

    Edits

--- script/code.js
+++ script/code.js
@@ -11,5 +11,18 @@ const Friends = {
 }
 
 console.log(Friends);
+console.table(Friends);
 
 });
+
+// Joel's Solution
+// let friends = []
+// let addFriends = document.querySelector('[addFriends]') 
+// addFriends.addEventListener('click', () => {
+//     friends.push({
+//         firstName: document.querySelector('[firstName]').value,
+//         lastName: document.querySelector('[lastName]').value
+        
+//     })
+//     console.log(friends);
+// })
